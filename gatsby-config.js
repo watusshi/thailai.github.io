454191021cdb9f393d2dabef2dbce1466e07fb2a
Thai Lai <khanhthailai@gmail.com>
2021-11-29 13:52:07 -0600

    source code

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Thai Lai',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Thai Lai | Full-Time Software Engineer Student`,
     // Description that goes under your name in main bio
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
     // Optional: Twitter account handle
