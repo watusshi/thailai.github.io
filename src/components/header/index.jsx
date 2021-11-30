2ca1cbc40bf3176f7eca32ebb4770d4d471d7cec
Thai Lai <khanhthailai@gmail.com>
2021-11-30 11:30:54 -0600

    source code

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -18,7 +18,7 @@ const classes = {
 };
 
 const Header = ({ metadata = {}, noBlog = false }) => {
-  const stackoverflow = get(metadata, 'stackoverflow', false);
+  const twitter = get(metadata, 'author', false);
   const github = get(metadata, 'github', false);
   const linkedin = get(metadata, 'linkedin', false);
 
