a23edceed9a46456738001b818c5259d66e0f254
Thai Lai <khanhthailai@gmail.com>
2022-07-28 13:26:27 -0500

    source code

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         name: 'Java and Donut Express (JADE)',
         description:
           'An application which allows business to run more effectively (easier communicate between customers and sellers)',
-        link: ,
+        link: '',
       },
       {
         name: 'ChromeExtensionKit',
